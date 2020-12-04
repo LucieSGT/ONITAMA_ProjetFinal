d21497e8220f1cccaeab445588c80fe5e9f8459d
bdarties <benoit.darties@gmail.com>
2020-12-04 11:58:41 +0100

    Commentaires sur votre code

--- src/projetonitama_sarinena_forssant/Plateau.java
+++ src/projetonitama_sarinena_forssant/Plateau.java
@@ -21,6 +21,8 @@ public class Plateau {
         }
     }
  boolean AjouterPion(){ // ajoute les 10 jetons sur le plateau, disposés d'une certaine manière
+     // BD : si on ajoute les 10 pions, il vaut mieux renommer la méthode ajouterPions avec un s, pour la distinguer
+     // sino non peut penser qu'on ajoute juste un pion. Meme : positionnerPionsDepart serait plus parlant
      return true;
  } 
  
@@ -29,20 +31,24 @@ public class Plateau {
  }
  
  boolean CaseOccupee(){ //renvoie si la case est occupée par un pion (adverse ou non)
+     // BD : quelle case ? il manque les coordonnées en paramètre
      return true;
  }
  
  String LireCouleurPion(){ // renvoie la couleur du pion 
+     // BD : la couleur de quel pion? celui qui est sur quelle cas? idem que précédent
      return null;
  }
  
  //methode alliant AjouterPion et SupprimerPion
  // utilisée lors de la capture d'un pion adverse
  boolean RemplacerPion(){ 
+     // BD : remplacer le pion de quelle case, par quoi ? manque coords et le nouveau pion
      return true;
  }
  
  boolean SupprimerPion(){ // supprime le pion du plateau
+     // BD : sur quelle case on supprime le pion ?
      return true;
  }
  
@@ -62,9 +68,13 @@ public class Plateau {
  
  // cases grises -> visualiser les potentiels mouvements de pions avec les cartes en main
  void PlacerCaseGrise(){
+     // BD : on place les case grises à partir d'un pion choisi par ses coordonnées, et d'une carte, non ?
+     // ou sont ces éléments quand on appelle cette méthode ?
      
  }
  
  //+Dragons... (16)
+
+
     
 }
